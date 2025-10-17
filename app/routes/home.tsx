028a922cfddca5a72e95d3052b613411745ddbe7
Aravind Kreshna <aravindkreshna@gmail.com>
2025-10-18 03:50:30 +0530

    navbar Component is included

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
+import Navbar from "~/components/navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ai Resume Analyzer" },
+    { name: "description", content: "Our Small Step to Land on Your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Welcome to Ai Resume Analyzer</h1>
+        <h2>Review your submissions and check AI-Powered Feedback.</h2>
+      </div>
+    </section>
+  </main>
 }
